bdbe8bb0c9a27780e9a209f5c7d27de40b631ed9
Jason Wong <wongkj.11@gmail.com>
2026-08-05 21:23:28 +1000

    feat: Creating an s3 bucket

--- lib/dinosaur-information-service-stack.ts
+++ lib/dinosaur-information-service-stack.ts
@@ -3,6 +3,7 @@ import * as cdk from "aws-cdk-lib";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as scheduler from "aws-cdk-lib/aws-scheduler";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import APIGatewayConstruct from "./constructs/APIGatewayConstruct";
 import { LambdaConstruct } from "./constructs/LambdaConstruct";
@@ -14,9 +15,18 @@ export class DinosaurInformationServiceStack extends cdk.Stack {
     const dinosaurInformationScheduleExpression = String(
       process.env.DINO_INFO_SCHEDULE ?? "cron(30 5 * * ? *)",
     );
+    const stage = String(process.env.STAGE ?? "dev").toLowerCase();
+    const dinosaurDataBucketBaseName = String(
+      process.env.DINO_DATA_BUCKET ?? "dino-data",
+    ).toLowerCase();
+    const dinosaurDataBucketName = `${stage}-${dinosaurDataBucketBaseName}`;
 
     const api = new APIGatewayConstruct(this, "dinosaur-info-api", {});
     const getDinoInfoApi = api.returnApi();
+
+    const dinosaurDataBucket = new s3.Bucket(this, "dinosaur-data-bucket", {
+      bucketName: dinosaurDataBucketName,
+    });
 
     const helloWorldLambda = new LambdaConstruct(this, "hello-world-lambda", {
       functionName: "hello-world-lambda",
@@ -113,6 +123,14 @@ export class DinosaurInformationServiceStack extends cdk.Stack {
       value:
         dinosaurInformationSchedule.name ??
         "dinosaur-information-daily-schedule",
+    });
+
+    new cdk.CfnOutput(this, "DinosaurDataBucketName", {
+      value: dinosaurDataBucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, "DinosaurDataBucketArn", {
+      value: dinosaurDataBucket.bucketArn,
     });
 
     new cdk.CfnOutput(this, "ExpressProxyLambdaName", {
